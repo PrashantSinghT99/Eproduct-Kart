840f1c9d64dd56f792b363a39a02877058b8f418
PrashantTariyal <52065013+PrashantTariyal@users.noreply.github.com>
2022-10-05 16:03:18 +0530

    4 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -6,20 +6,23 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CartService {
 
+  //items array
   items: Product[] = [];
   
+//method to add new component to the cart
   addToCart(product: Product) {
     this.items.push(product);
   }
-
+//method for fetching the cart items
   getItems() {
     return this.items;
   }
-
+//method for emptying the cart
   clearCart() {
     this.items = [];
     return this.items;
   }
+  //method to get shipping prices with http from json file
   getShippingPrices() {
     return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-shipping',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shipping.component.css']
 })
 export class ShippingComponent implements OnInit {
+  shippingCosts!: Observable<{ type: string, price: number }[]>;
 
-  constructor() { }
+  constructor(private cartService:CartService) { }
+
+// calling this function from cartService to get shipping prices
 
   ngOnInit(): void {
+
+    this.shippingCosts=this.cartService.getShippingPrices();
+
   }
 
 }
