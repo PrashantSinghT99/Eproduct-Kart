b2cdc1b6b8b8b1ddec30372cdcd2488a965d6d6f
PrashantTariyal <52065013+PrashantTariyal@users.noreply.github.com>
2022-10-06 11:03:17 +0530

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
-
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -8,8 +8,25 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit {
   items = this.cartService.getItems();
-  //injecting cart service to getItems from cartService.ts
-  constructor(private cartService: CartService) {}
 
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+
+  //injecting cart service to getItems from cartService.ts
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
+  onclickPurchase() {
+    window.alert('Purchased Item will be shipped in 2 working days!!');
+  }
   ngOnInit(): void {}
 }
